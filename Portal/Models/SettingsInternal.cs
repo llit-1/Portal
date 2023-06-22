c26e414f283658f724cbf3c69a01dd7dbb5a5756
Unknown <it@ludilove.ru>
2023-06-22 12:48:43 +0300

    Portal v2.14.3.3
    Запретил заказывать более 1 картриджа в 1 заказе

--- Portal/Models/SettingsInternal.cs
+++ Portal/Models/SettingsInternal.cs
@@ -8,7 +8,7 @@ namespace Portal.Models
     public class SettingsInternal
     {
         // Версия
-        public static string portalVersion = "2.14.3.2";
+        public static string portalVersion = "2.14.3.3";
 
         // Текущая конфигурация приложения
         public static Microsoft.Extensions.Configuration.IConfiguration Configuration;
