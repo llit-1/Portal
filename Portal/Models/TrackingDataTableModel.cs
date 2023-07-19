dd8b92df5726f310ed4d7681927b4e6259e3621b
Unknown <it@ludilove.ru>
2023-07-19 09:34:41 +0300

    Portal v2.14.4.8(alfa)
    1) Реализована страница просмотра данных "Учет табеля рабочего времени"

--- Portal/Models/TrackingDataTableModel.cs
+++ Portal/Models/TrackingDataTableModel.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections.Generic;
 using System;
 
@@ -29,3 +30,34 @@ namespace Portal.Models
             public List<Models.MSSQL.TimeSheet> TimeSheet { get; set; }
         }
 }
+=======
+﻿using Microsoft.CodeAnalysis;
+using Portal.Models.MSSQL;
+using System;
+using System.Collections.Generic;
+
+namespace Portal.Models
+{
+    public class TrackingDataModel
+    {
+        public List<TTData> TTDatas { get; set; }
+
+        public DateTime Begin { get; set; }
+
+        public DateTime End { get; set; }
+
+        public List<MSSQL.Location.Location> Location { get; set; } 
+    }
+    public class TTData
+    {
+        public Portal.Models.MSSQL.Location.Location Location { get; set; }
+        public List<DateData> DateDatas { get; set; }
+    }
+
+    public class DateData
+    {
+        public DateTime Date { get; set; }
+        public List<TimeSheet> TimeSheets { get; set; }
+    }
+}
+>>>>>>> 698b6dd... Portal v2.14.4.4
