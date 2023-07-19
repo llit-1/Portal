139dbe832e84b26015cbf594c932bae2a0a4f353
Unknown <it@ludilove.ru>
2023-07-19 11:44:27 +0300

    Portal v2.14.4.10(alfa)
    Добавлен метод TrackingDataEdit

--- Portal/Controllers/TimeTrackingController.cs
+++ Portal/Controllers/TimeTrackingController.cs
@@ -10,6 +10,7 @@ using System.Security.Claims;
 using Portal.Models.Calculator;
 using Portal.Models;
 using Microsoft.CodeAnalysis;
+using System.Drawing;
 
 namespace Portal.Controllers
 {
@@ -97,72 +98,26 @@ namespace Portal.Controllers
         {
             return PartialView();
         }
-        public IActionResult TrackingDataEdit(string begin, string end, string locationguid)
+        public IActionResult TrackingDataEdit(string stringDate, string locationGuid)
         {
-            DateTime beginDateTime = new();
-            DateTime endDateTime = new();
-            TrackingDataModel trackingDataModel = new TrackingDataModel();
-            trackingDataModel.TTDatas = new();
-            if (begin == null || end == null)
-            {
-                beginDateTime = DateTime.Now.Date;
-                endDateTime = DateTime.Now.Date;
-            }
-            else
-            {
-                beginDateTime = DateTime.ParseExact(begin, "yyyy-MM-dd", null);
-                endDateTime = DateTime.ParseExact(end, "yyyy-MM-dd", null);
-            }
-            trackingDataModel.Begin = beginDateTime;
-            trackingDataModel.End = endDateTime;
-            List<DateTime> dateList = new List<DateTime>();
-            for (DateTime i = beginDateTime; i <= endDateTime; i = i.AddDays(1))
-            {
-                dateList.Add(i);
-            }
-            string userLogin = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.WindowsAccountName).Value;
-            RKNet_Model.Account.User user = db.Users.Include(c => c.TTs.Where(d => d.CloseDate == null && d.Type != null && d.Type.Id != 3))
-                                                    .FirstOrDefault(c => c.Login == userLogin);
-            trackingDataModel.Location = new();
-            foreach (var tt in user.TTs)
-            {
-                var location = dbSql.Locations.FirstOrDefault(c => c.RKCode == tt.Restaurant_Sifr);
-                if (location == null) { continue; }
-                trackingDataModel.Location.Add(location);
-            }
-            List<RKNet_Model.TT.TT> tts = new();
-            if (!string.IsNullOrEmpty(locationguid) && locationguid != "0")
-            {
-                Portal.Models.MSSQL.Location.Location location = dbSql.Locations.FirstOrDefault(c => c.Guid == Guid.Parse(locationguid));
-                RKNet_Model.TT.TT tt = db.TTs.FirstOrDefault(c => c.Restaurant_Sifr == location.RKCode);
-                tts.Add(tt);
-            }
-            else
-            {
-                tts = user.TTs;
-            }
-            foreach (var TT in tts)
-            {
-                Portal.Models.MSSQL.Location.Location location = dbSql.Locations.FirstOrDefault(c => TT.Restaurant_Sifr == c.RKCode);
-                if (location == null) { continue; }
-                TTData tTData = new TTData();
-                tTData.Location = location;
-                tTData.DateDatas = new();
-                foreach (var date in dateList)
-                {
-                    DateData dateData = new DateData();
-                    dateData.Date = date;
-                    dateData.TimeSheets = dbSql.TimeSheets.Include(c => c.Personality)
-                                                          .Include(c => c.Location)
-                                                          .Include(c => c.JobTitle)
-                                                          .Where(c => c.Location == location && c.Begin > date && c.Begin < date.AddDays(1))
-                                                          .ToList();
-                    tTData.DateDatas.Add(dateData);
-                }
-                trackingDataModel.TTDatas.Add(tTData);
-            }
-
-            return PartialView(trackingDataModel);
+            TTData tTData = new TTData();
+            DateTime date = DateTime.ParseExact(stringDate, "yyyy-MM-dd", null);
+            Portal.Models.MSSQL.Location.Location location = dbSql.Locations.FirstOrDefault(c => c.Guid == Guid.Parse(locationGuid));
+            DateData dateData = new DateData();
+            dateData.Date = date;
+            dateData.TimeSheets = dbSql.TimeSheets.Include(c => c.Personality)
+                                                  .Include(c => c.JobTitle)
+                                                  .Where(c => c.Begin.Date == date && c.Location.Guid == location.Guid).ToList();
+            tTData.Location = location;
+            tTData.DateDatas = new List<DateData> { dateData };
+            TrackingDataEditModel trackingDataEditModel = new TrackingDataEditModel();
+            trackingDataEditModel.TTData = tTData;
+            trackingDataEditModel.Personalities = dbSql.Personalities.Include(c => c.JobTitle)
+                                                                     .Include(c => c.Schedule)
+                                                                     .Include(c => c.Location)
+                                                                     .ToList();
+            trackingDataEditModel.JobTitles = dbSql.JobTitles.ToList();
+            return PartialView(trackingDataEditModel);
         }
 
 
---
+++ Portal/Models/TrackingDataEditModel.cs
@@ -0,0 +1,12 @@
+﻿using Portal.Models.MSSQL.Personality;
+using System.Collections.Generic;
+
+namespace Portal.Models
+{
+    public class TrackingDataEditModel
+    {
+        public TTData TTData { get; set; }
+        public List<Personality> Personalities { get; set; }
+        public List<JobTitle> JobTitles { get; set; }
+    }
+}
