f7f574dd9d6bf83b47131f2554887c50f5de65e0
kukuFs <kurg5250@gmail.com>
2025-08-11 17:01:16 +0300

    Portal v2.14.16.3 Fix

--- Portal/Models/SettingsInternal.cs
+++ Portal/Models/SettingsInternal.cs
@@ -8,7 +8,7 @@ namespace Portal.Models
     public class SettingsInternal
     {
         // Версия
-        public static string portalVersion = "2.14.16.2";
+        public static string portalVersion = "2.14.16.3";
 
         // Текущая конфигурация приложения
         public static Microsoft.Extensions.Configuration.IConfiguration Configuration;
