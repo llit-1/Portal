fd1311b358f0906ce75856f969bdd7eb4c952096
Александр <alexspb41@gmail.com>
2025-08-27 11:30:13 +0300

    Portal v 2.14.16.5 build 5
    Добавлены функции для вида создания нового FactoryPerson

--- Portal/Controllers/PersonalityFactoryController.cs
+++ Portal/Controllers/PersonalityFactoryController.cs
@@ -1,3 +1,4 @@
+using DocumentFormat.OpenXml.Bibliography;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -49,7 +50,61 @@ namespace Portal.Controllers
         [Authorize(Roles = "employee_control_factory")]
         public IActionResult PersonalityFactoryAdd()
         {
-            return PartialView();
+            PersonalityFactoryAddModel personalityFactoryAddModel = new PersonalityFactoryAddModel();
+            personalityFactoryAddModel.FactoryDepartments = dbSql.FactoryDepartment.ToList();
+            personalityFactoryAddModel.FactoryEntities = dbSql.FactoryEntity.ToList();
+            personalityFactoryAddModel.FactoryCitizenshipTypes = dbSql.FactoryCitizenshipType.ToList();
+            personalityFactoryAddModel.FactoryDocumentTypes = dbSql.FactoryDocumentType.ToList();
+            personalityFactoryAddModel.FactoryBanks = dbSql.FactoryBanks.ToList();
+            return PartialView(personalityFactoryAddModel);
+        }
+
+        [Authorize(Roles = "employee_control_factory")]
+        public IActionResult GetWorkshops(int depertment)
+        {
+           List<FactoryWorkshop> factoryWorkshops = dbSql.FactoryDepartmentFactoryWorkshop
+                                                          .Include(x => x.FactoryWorkshop)
+                                                          .Where(x => x.FactoryDepartmentId == depertment)
+                                                          .Select(x => x.FactoryWorkshop).ToList();
+            return Ok(factoryWorkshops);
+        }
+
+        public IActionResult GetJobTitles(int depertment, int workshop)
+        {
+            List<FactoryJobTitle> factoryJobTitles = dbSql.FactoryDepartmentWorkshopJobTitle
+                                                           .Include(x => x.JobTitleWorkshop)
+                                                           .ThenInclude(a => a.FactoryJobTitle)
+                                                           .Where(x => x.FactoryDepartmentId == depertment && x.FactoryWorkshopId == workshop)
+                                                           .Select(x => x.JobTitleWorkshop.FactoryJobTitle).ToList();
+            return Ok(factoryJobTitles);
+        }
+        public IActionResult GetCitizenship(int citizenshipType)
+        {
+            List<FactoryCitizenship> factoryCitizenships = dbSql.FactoryCitizenship.Where(x => x.CitizenshipTypeId == citizenshipType).ToList();
+            return Ok(factoryCitizenships);
+        }
+
+        public IActionResult SaveNewPerson([FromBody] FactoryPerson person)
+        {
+            if (person == null)
+            {
+                return BadRequest(new { Message = "person is null" });
+            }
+            dbSql.FactoryPerson.Add(person);
+            dbSql.SaveChanges();
+            return Ok();
+        }
+
+
+
+
+        public class PersonalityFactoryAddModel
+        {
+            public List<FactoryDepartment> FactoryDepartments { get; set; } = new List<FactoryDepartment>();
+            public List<FactoryEntity> FactoryEntities { get; set; } = new List<FactoryEntity>();
+            public List<FactoryCitizenshipType> FactoryCitizenshipTypes { get; set; } = new List<FactoryCitizenshipType>();
+            public List<FactoryDocumentType> FactoryDocumentTypes { get; set; } = new List<FactoryDocumentType>();
+            public List<FactoryBanks> FactoryBanks { get; set; } = new List<FactoryBanks>();
         }
     }
 }
