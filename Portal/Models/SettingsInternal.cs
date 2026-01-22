d0f599fa7ee9f1469e0e543baa48035ff96acf02
kukuFs <kurg5250@gmail.com>
2026-01-22 17:10:51 +0300

    Portal v2.15.8.9
    Исправлено отображение актуального имени пользователя

--- Portal/Models/SettingsInternal.cs
+++ Portal/Models/SettingsInternal.cs
@@ -8,7 +8,7 @@ namespace Portal.Models
     public class SettingsInternal
     {
         // Версия
-        public static string portalVersion = "2.15.8.8";
+        public static string portalVersion = "2.15.8.9";
 
         // Текущая конфигурация приложения
         public static Microsoft.Extensions.Configuration.IConfiguration Configuration;
